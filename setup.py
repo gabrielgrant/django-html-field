d8ad8fb10af680c8648b6117415c6658f074cf1e
Gabriel Grant <g@briel.ca>
2010-12-30 18:38:22 -0500

    switch from distutils to setuptools to use include_package_data=True

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='django-html-field',
     version='0.1dev',
     packages=['html_field',],
+    include_package_data=True,
     author='Gabriel Grant',
     author_email='g@briel.ca',
     license='LGPL',
