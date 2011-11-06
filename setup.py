be9e51c0b4a67f9c231fbb6f77a4b8437e40bba9
Gabriel Grant <g@briel.ca>
2011-11-06 10:03:23 -0800

    Install subpackages with setup.py
    Fixes #1
    Thanks (merci :) @domguard for the report

--- setup.py
+++ setup.py
@@ -2,8 +2,14 @@ from setuptools import setup
 
 setup(
     name='django-html-field',
-    version='0.1.2dev',
-    packages=['html_field',],
+    version='0.1.3',
+    packages=[
+        'html_field',
+        'html_field.db',
+        'html_field.db.models',
+        'html_field.forms',
+        'html_field.tests',
+    ],
     include_package_data=True,
     author='Gabriel Grant',
     author_email='g@briel.ca',
